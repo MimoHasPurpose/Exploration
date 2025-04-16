f9d6545c9fd9a5b85ce8ef59af00ae87dd7eac71
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-04-16 06:19:43 +0530

    wealth model

--- Mesa/econ/main.py
+++ Mesa/econ/main.py
@@ -11,25 +11,45 @@ class MoneyAgent(mesa.Agent):
         super().__init__(model)
         self.wealth=1
     
-    def say_hi(self):
-         print(f"Hi, I am an agent, you can call me cutie no: {self.unique_id!s}.")
-    
+    def exchange(self):
+        if self.wealth>0:
+            other_agent=self.random.choice(self.model.agents)
+            if other_agent is not None:
+                other_agent.wealth+=1
+                self.wealth-=1
+
     def say_wealth(self):
-        print(f"Hi boss am{self.unique_id!s} my wealth is huhuhu only: {self.wealth}")
+        print(f"Hi boss am {self.unique_id!s} my wealth is huhuhu only: {self.wealth} ")
          
 
 
 
 class MoneyModel(mesa.Model):
-    def __init__(self,n,seed=42):
-        super().__init__(seed=seed)
+    def __init__(self,n):
+        super().__init__()
         self.num_agents=n 
         MoneyAgent.create_agents(model=self, n=n)
 
     def step(self):
         self.agents.do("say_wealth")
-        self.agents.do("say_hi")
+        self.agents.shuffle_do("exchange")
 
 
-starter_model = MoneyModel(5)
-starter_model.step()
+all_wealth = []
+# This runs the model 100 times, each model executing 10 steps.
+for _ in range(10):
+    # Run the model
+    model = MoneyModel(10)
+    for _ in range(30):
+        model.step()
+
+    # Store the results
+    for agent in model.agents:
+        all_wealth.append(agent.wealth)
+
+import matplotlib.pyplot as plt
+
+# Use seaborn
+g = sns.histplot(all_wealth, discrete=True)
+g.set(title="Wealth distribution", xlabel="Wealth", ylabel="number of agents")
+plt.show()
